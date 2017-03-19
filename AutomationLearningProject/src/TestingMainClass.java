44503b3700f1fc4bbf1ff8b70210da8f56bc87a7
Pankaj Pant <Pankaj Pant@LAPTOP-798PJ03R>
2017-03-19 11:32:48 +0530

    Change main

--- AutomationLearningProject/src/TestingMainClass.java
+++ AutomationLearningProject/src/TestingMainClass.java
@@ -7,7 +7,7 @@
  *
  */
 public class TestingMainClass {
- public static void mail(String args[]) {
+ public static void main(String args[]) {
 	 System.out.println("TODO:implement testing practices");
  }
 	 
